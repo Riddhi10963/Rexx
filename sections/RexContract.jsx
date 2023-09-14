ea66715c7e2f456e03b21737629135e04e6fb882
Riddhi10963 <riddhijais92@gmail.com>
2023-09-15 02:42:32 +0530

    rexx

--- sections/RexContract.jsx
+++ sections/RexContract.jsx
@@ -16,12 +16,30 @@ const RexContract = () => {
         viewport={{ once: false, amount: 0.25 }}
         className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
       >
-        <TitleTextTyping title="Rexx Contract" textStyle="text-center" />
+        <TitleTextTyping title="Rexx Tokenomics" textStyle="text-center" />
         <motion.p
           variants={fadeIn("up", "tween", 0.2, 1)}
-          className="mt-20 md:px-10 font-normal border p-4  rounded text-sm sm:text-base text-center  text-[#9548ee]  shadow-gray-200 hover:shadow-purple-400"
+          className=" w-[95%] lg:w-1/2 mt-20 md:px-10 font-normal border py-4  rounded text-sm sm:text-base text-center  text-white  shadow-gray-200 hover:shadow-purple-400"
         >
-          0xfd140b8d01ceaa28defb38b837b7928c
+          Contract address:- 0xfd140b8d01ceaa28defb38b837b79
+        </motion.p>
+        <motion.p
+          variants={fadeIn("up", "tween", 0.2, 1)}
+          className=" w-[95%] lg:w-1/2 mt-10 md:px-10 font-normal border py-4  rounded text-sm sm:text-base text-center  text-white  shadow-gray-200 hover:shadow-purple-400"
+        >
+          Token Symbol:- REXX
+        </motion.p>
+        <motion.p
+          variants={fadeIn("up", "tween", 0.2, 1)}
+          className=" w-[95%] lg:w-1/2 mt-10 md:px-10 font-normal border py-4  rounded text-sm sm:text-base text-center  text-white  shadow-gray-200 hover:shadow-purple-400"
+        >
+          Decimal:- 18{" "}
+        </motion.p>
+        <motion.p
+          variants={fadeIn("up", "tween", 0.2, 1)}
+          className=" w-[95%] lg:w-1/2 mt-10 md:px-10 font-normal border py-4  rounded text-sm sm:text-base text-center  text-white  shadow-gray-200 hover:shadow-purple-400"
+        >
+          Total Suppy:-700 Million
         </motion.p>
       </motion.div>
     </section>
